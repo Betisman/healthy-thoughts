84c8b80765d96fa04b00197906ed3c5b73f1cda0
jonatan-martinez <102022630+jonatan-martinez@users.noreply.github.com>
2022-06-24 16:21:36 +0200

    feat: conversation

--- components/slack/initSlack.js
+++ components/slack/initSlack.js
@@ -4,6 +4,8 @@ module.exports = () => {
     let app
     const start = async ({config, logger}) => {
         const {botToken: token, appToken, signingSecret} = config
+        let friendUserChannel = ''
+        let establishedRelation = false
         app = new App({
             token,
             signingSecret,
@@ -11,42 +13,63 @@ module.exports = () => {
             appToken,
             port: process.env.PORT || 3000
           });
-
+          const privateChannelID = "D03LUMEL7F0"
           app.message(/.*/, async (params) => {
             const {message, say, client} = params;
-            await say(`Hey there <@${message.user}>! I've let the *MHFA Team* know, they'll contact you soonish!! :smiley:`);
-            logger.info(`Hey there <@${message.user}>! I've let the *MHFA Team* know, they'll contact you soonish!! :smiley:`);
-            await client.chat.postMessage({
-              token,
-              channel: 'C03LM3LEYP9',
-              text: `A colleague wants to chat to a MHFA, they said: "${message.text}"`,
-              blocks: [
-                {
-                  type: 'section',
-                  text: {
-                    type: 'mrkdwn',
-                    text: `A colleague wants to chat to a MHFA, they said: "${message.text}"`,
+            if (!establishedRelation) {
+              friendUserChannel = message.channel
+              await say(`Hey there <@${message.user}>! I've let the *MHFA Team* know, they'll contact you soonish!! :smiley:`);
+              logger.info(`Hey there <@${message.user}>! I've let the *MHFA Team* know, they'll contact you soonish!! :smiley:`);
+              await client.chat.postMessage({
+                token,
+                channel: 'C03LM3LEYP9',
+                text: `Some mate wants to contact us: he/she has contacted us with this message ${message.text}`,
+                blocks: [
+                  {
+                    type: 'section',
+                    text: {
+                      type: 'mrkdwn',
+                      text: `Some mate wants to contact by saying ${message.text}`,
+                    },
+                    block_id: 'text1'
                   },
-                  block_id: 'text1'
-                },
-                {
-                  type: 'actions',
-                  block_id: 'accept_healthy_thoughts',
-                  elements: [
-                    {
-                      type: 'button',
-                      text: {
-                        type: 'plain_text',
-                        emoji: true,
-                        text: `I'll help :thumsbup:`,
-                      },
-                      style: 'primary',
-                      value: 'Accept'
-                    }
-                  ]
-                }
-              ]
-            })
+                  {
+                    type: 'actions',
+                    block_id: 'accept_healthy_thoughts',
+                    elements: [
+                      {
+                        type: 'button',
+                        text: {
+                          type: 'plain_text',
+                          emoji: true,
+                          text: `I'll talk to our anonymous mate`,
+                        },
+                        style: 'primary',
+                        value: 'Accept'
+                      }
+                    ]
+                  }
+                ]
+              })
+            } else {
+              let channelToUse = message.channel === privateChannelID ? friendUserChannel : privateChannelID
+              await client.chat.postMessage({
+                token,
+                channel: channelToUse,
+                text: message.text,
+                blocks: [
+                  {
+                    type: 'section',
+                    text: {
+                      type: 'mrkdwn',
+                      text: message.text,
+                    },
+                    block_id: 'text1'
+                  }
+                ]
+              })
+            }
+
           });
 
           app.action({block_id: 'accept_healthy_thoughts'}, async (params) => {
