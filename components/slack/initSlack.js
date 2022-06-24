2b7aba4465aab206001acea28de6d1b348cd4bcf
jonatan-martinez <102022630+jonatan-martinez@users.noreply.github.com>
2022-06-24 17:02:52 +0200

    enable app

--- components/slack/initSlack.js
+++ components/slack/initSlack.js
@@ -3,7 +3,6 @@ const { App } = require('@slack/bolt');
 module.exports = () => {
     let app
     const start = async ({config, logger}) => {
-        if (process.env.SERVICE_ENV !== 'local') return
         const {botToken: token, appToken, signingSecret} = config
         let friendUserChannel = ''
         let establishedRelation = false
