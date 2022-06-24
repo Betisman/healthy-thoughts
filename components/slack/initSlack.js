3b4a4b08e66a6a67fda77d434b32fc049aebe71b
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2022-06-24 15:44:32 +0200

    typing like a native

--- components/slack/initSlack.js
+++ components/slack/initSlack.js
@@ -19,13 +19,13 @@ module.exports = () => {
             await client.chat.postMessage({
               token,
               channel: 'C03LM3LEYP9',
-              text: `Some mate wants to contact us: he/she has contacted us with this message ${message.text}`,
+              text: `A colleague wants to chat to a MHFA, they said: "${message.text}"`,
               blocks: [
                 {
                   type: 'section',
                   text: {
                     type: 'mrkdwn',
-                    text: `Some mate wants to contact by saying ${message.text}`,
+                    text: `A colleague wants to chat to a MHFA, they said: "${message.text}"`,
                   },
                   block_id: 'text1'
                 },
@@ -38,7 +38,7 @@ module.exports = () => {
                       text: {
                         type: 'plain_text',
                         emoji: true,
-                        text: `I'll talk to our anonymous mate`,
+                        text: `I'll help :thumsbup:`,
                       },
                       style: 'primary',
                       value: 'Accept'
@@ -51,7 +51,7 @@ module.exports = () => {
 
           app.action({block_id: 'accept_healthy_thoughts'}, async (params) => {
             const {say} = params
-            say('You are now connected to you anonymous peer')
+            say('You\'re now anonymously connected to a MFHA');
           })
 
           await app.start();
